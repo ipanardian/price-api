3e9de9e8bf47b3fccef92f04eec11d2f4de0548d
Ipan Ardian <ardian.ipan@gmail.com>
2024-10-29 10:20:36 +0700

    feat: enhance speed authentication

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/ipanardian/price-api/internal/cache"
 	"github.com/ipanardian/price-api/internal/constant"
 	dtoV1 "github.com/ipanardian/price-api/internal/dto/v1"
 	"github.com/ipanardian/price-api/internal/helpers"
@@ -39,15 +41,24 @@ func AuthApiMiddleware() fiber.Handler {
 			})
 		}
 
-		_apiSecretDe, err := helpers.DecryptAES256CBC(_aesKey, _apiSecretEn)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(dtoV1.ResponseWrapper{
-				Status:        constant.RequestFailure,
-				StatusCode:    "500",
-				StatusNumber:  constant.DecryptAESError,
-				StatusMessage: "Sorry, this is not working properly. We know about this mistake and are working to fix it.",
-				Timestamp:     time.Now().Unix(),
-			})
+		var _apiSecretDe string
+		_apiSecretDeMem, f := cache.MemGet(fmt.Sprintf("scrt.%s", apiKey))
+		if !f {
+			var err error
+			_apiSecretDe, err = helpers.DecryptAES256CBC(_aesKey, _apiSecretEn)
+			if err != nil {
+				return c.Status(fiber.StatusInternalServerError).JSON(dtoV1.ResponseWrapper{
+					Status:        constant.RequestFailure,
+					StatusCode:    "500",
+					StatusNumber:  constant.DecryptAESError,
+					StatusMessage: "Sorry, this is not working properly. We know about this mistake and are working to fix it.",
+					Timestamp:     time.Now().Unix(),
+				})
+			}
+
+			cache.MemSet(fmt.Sprintf("scrt.%s", apiKey), _apiSecretDe, 60*time.Minute)
+		} else {
+			_apiSecretDe = _apiSecretDeMem.(string)
 		}
 
 		signature := helpers.GenerateSignature(_apiSecretDe)
